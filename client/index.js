063e50a6ba21e6a586e48a9a65c3deee458c921c
Dylan Piercey <pierceydylan@gmail.com>
2016-10-23 13:45:59 -0700

    Fix browser side usage of single middlewares

--- client/index.js
+++ client/index.js
@@ -1,4 +1,19 @@
 'use strict'
 
-// Helmet middleware is a noop in the browser.
-module.exports = function noop () {}
+// All helmet middlewares are a noops in the browser.
+module.exports = helmet
+helmet.contentSecurityPolicy =
+helmet.dnsPrefetchControl =
+helmet.frameguard =
+helmet.hidePoweredBy =
+helmet.hpkp =
+helmet.hsts =
+helmet.ieNoOpen =
+helmet.noCache =
+helmet.noSniff =
+helmet.referrerPolicy =
+helmet.xssFilter = noop
+
+// Does nothing
+function helmet () {}
+function noop () {}
